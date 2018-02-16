63702d20855d829903a9ea1f2b38037a00558e23
dzhigimont <dzhigimont@mail.ru>
2018-02-16 20:08:38 +0300

    Change Mysql database url to heroku database and change Redis connection to heroku Redis

--- src/main/java/com/zhigimont/bookstore/config/HttpSessionConfig.java
+++ src/main/java/com/zhigimont/bookstore/config/HttpSessionConfig.java
@@ -1,15 +1,44 @@
 package com.zhigimont.bookstore.config;
 
 
+import jdk.nashorn.internal.objects.annotations.Property;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
+@PropertySource("application.properties")
 public class HttpSessionConfig {
+    @Value("${spring.redis.host}")
+    private String redisHostName;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+
 
     @Bean
-    public LettuceConnectionFactory connectionFactory(){
-        return new LettuceConnectionFactory();
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setHostName(redisHostName);
+        factory.setPort(redisPort);
+        factory.setPassword(redisPassword);
+        factory.setUsePool(true);
+        return factory;
     }
+
+    @Bean
+    public static ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
+
+
+
 }
